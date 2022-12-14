faf2810048d11a8311f52af722270e158ad4d638
Velavee <josephsg@oregonstate.edu>
2022-12-13 18:55:00 -0800

    chore: first commit

---
+++ main.py
@@ -0,0 +1,74 @@
+import sys
+import re, numpy as np, pandas as pd
+from pprint import pprint
+
+# Gensim
+import gensim, spacy, logging, warnings
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+import matplotlib.pyplot as pyplot
+
+# NLTK Stop words
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+
+warnings.filterwarnings('ignore',category=DeprecationWarning)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+def sent_to_words(titles):
+    for title in titles:
+        title = re.sub('\S*@\S*\s?', '', title)  # remove emails
+        title = re.sub('\s+', ' ', title)  # remove newline chars
+        title = re.sub("\'", "", title)  # remove single quotes
+        title = gensim.utils.simple_preprocess(str(title), deacc=True) 
+        yield(title)
+
+
+df = pd.read_json('testomatTests.json')
+print(df.shape)
+df.head()
+
+data = df.title.values.tolist()
+data_words = list(sent_to_words(data))
+
+# Build the bigram and trigram models
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100)
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+def process_words(texts, stop_words=stop_words, allowed_postags=['NOUN','ADJ','VERB','ADV']):
+    texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    texts = [bigram_mod[doc] for doc in texts]
+    texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
+    texts_out = []
+    nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+    for title in texts:
+        doc = nlp(" ".join(title))
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    return texts_out
+
+data_ready = process_words(data_words)
+
+# Create Dictionary
+id2word = corpora.Dictionary(data_ready)
+
+# Create Corpus: Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in data_ready]
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                            id2word=id2word,
+                                            num_topics=4,
+                                            random_state=100,
+                                            update_every=1,
+                                            chunksize=10,
+                                            passes=10,
+                                            alpha='symmetric',
+                                            iterations=100,
+                                            per_word_topics=True)
+
+pprint(lda_model.print_topics())
+
